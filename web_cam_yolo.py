d3164561023c6a0c0643004b838fabeedac389df
Manas Kalra <97355389+Mank-isnoob@users.noreply.github.com>
2023-06-17 22:23:52 +0530

    image_object_detection.py

--- web_cam_yolo.py
+++ web_cam_yolo.py
@@ -1,6 +1,6 @@
 from super_gradients.training import models
 
-yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco")
+yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco") # imports our pre-trained model
 
-url = "cars.jpg"
-yolo_nas_l.predict(url, conf=0.25).show()   #this can predict all models with yolo_nas_l
+url = "cars.jpg" # you can use your own file for image for detection
+yolo_nas_l.predict(url, conf=0.25).show()   #this can predict all classes from coco with yolo_nas_l
